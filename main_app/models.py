0795e325f857c7a7f376521199e6c3c6d413e301
TuckTuckC <mrkittay847@gmail.com>
2021-06-15 12:27:55 -0400

    adds models for user and product

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    name = models.CharField('Name', max_length=240)
+    email = models.EmailField()
+    wishlist = models.ForeignKey(product, on_delete=models.CASCADE)
+    registationDate = models.DateField('Registration Date', auto_now_add = True)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField('Name', maxLength=240)
+    #FOR WHEN WE HAVE IMAGE UPLOADS>> picture = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100)
