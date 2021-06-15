2d3338d2db9d191dbeec3561f84aefb946b0d275
TuckTuckC <mrkittay847@gmail.com>
2021-06-15 12:54:26 -0400

    adds review model

--- main_app/models.py
+++ main_app/models.py
@@ -4,12 +4,34 @@ from django.db import models
 class User(models.Model):
     name = models.CharField('Name', max_length=240)
     email = models.EmailField()
-    wishlist = models.ForeignKey(product, on_delete=models.CASCADE)
+    wishlist = models.ForeignKey('Product', on_delete=models.CASCADE)
     registationDate = models.DateField('Registration Date', auto_now_add = True)
 
     def __str__(self):
         return self.name
 
+
 class Product(models.Model):
     name = models.CharField('Name', maxLength=240)
+    brand = models.CharField('Brand', max_length=240)
     #FOR WHEN WE HAVE IMAGE UPLOADS>> picture = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100)
+    price = models.CharField('Price', max_length=240)
+    size = models.CharField('Size', max_length=240)
+    rms = models.CharField('RMS', max_length=240)
+    peak = models.CharField('Peak', max_length=240)
+    description = models.CharField('Description', max_length=500)
+    reviews = models.ForeignKey('Review', on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.name
+
+
+class Review(models.Model):
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    title = models.CharField('Title', max_length=240)
+    body = models.CharField('Body', max_length=500)
+    rating = models.IntegerField()
+
+    def __str__(self):
+        return self.title
