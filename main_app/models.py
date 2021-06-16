1f09e2b0222f367ee2f48c6d99d4d88db29dd226
TuckTuckC <mrkittay847@gmail.com>
2021-06-16 00:29:08 -0400

    changed product model to sub

--- main_app/models.py
+++ main_app/models.py
@@ -4,14 +4,14 @@ from django.db import models
 class User(models.Model):
     name = models.CharField('Name', max_length=240)
     email = models.EmailField()
-    wishlist = models.ForeignKey('Product', on_delete=models.CASCADE)
+    wishlist = models.ForeignKey('Sub', on_delete=models.CASCADE)
     registationDate = models.DateField('Registration Date', auto_now_add = True)
 
     def __str__(self):
         return self.name
 
 
-class Product(models.Model):
+class Sub(models.Model):
     name = models.CharField('Name', max_length=240)
     brand = models.CharField('Brand', max_length=240)
     #FOR WHEN WE HAVE IMAGE UPLOADS>> picture = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100)
