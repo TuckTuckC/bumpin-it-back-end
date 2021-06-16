00466152d14776e4dde0897ccbd2bf4db275a82e
TuckTuckC <mrkittay847@gmail.com>
2021-06-16 18:47:22 -0400

    added more specific models

--- main_app/models.py
+++ main_app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    name = models.CharField('Name', max_length=240)
+    name = models.CharField(max_length=240)
     email = models.EmailField()
     wishlist = models.ForeignKey('Sub', on_delete=models.CASCADE)
     registationDate = models.DateField('Registration Date', auto_now_add = True)
@@ -11,17 +11,51 @@ class User(models.Model):
         return self.name
 
 
-class Sub(models.Model):
-    name = models.CharField('Name', max_length=240)
-    brand = models.CharField('Brand', max_length=240)
-    #FOR WHEN WE HAVE IMAGE UPLOADS>> picture = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100)
-    price = models.CharField('Price', max_length=240)
-    size = models.CharField('Size', max_length=240)
-    rms = models.CharField('RMS', max_length=240)
-    peak = models.CharField('Peak', max_length=240)
-    description = models.CharField('Description', max_length=500)
+class SubPost(models.Model):
+    name = models.CharField(max_length=240)
+    brand = models.CharField(max_length=240)
+    #FOR WHEN WE HAVE IMAGE UPLOADS>> picture = models.ImageField(upload_to=None, height_field=None, width_fielmax_length=100)
+    price = models.CharField(max_length=240)
+    size = models.CharField(max_length=240)
+    rms = models.CharField(max_length=240)
+    peak = models.CharField(max_length=240)
+    description = models.CharField(max_length=500)
     reviews = models.ForeignKey('Review', on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.name
+
+
+class Sub(models.Model):
+    name = models.CharField(max_length=240)
+    brand = models.CharField(max_length=240)
+    #FOR WHEN WE HAVE IMAGE UPLOADS>> picture = models.ImageField(upload_to=None, height_field=None, width_fielmax_length=100)
+    size = models.CharField(max_length=240)
+    rms = models.CharField(max_length=240)
+    peak = models.CharField(max_length=240)
+
+    def __str__(self):
+        return self.name
+
+
+class AmpPost(models.Model):
+    name = models.CharField(max_length=240)
+    brand = models.CharField(max_length=240)
+    #FOR WHEN WE HAVE IMAGE UPLOADS>> picture = models.ImageField(upload_to=None, height_field=None, width_fielmax_length=100)
+    price = models.CharField(max_length=240)
+    wattage = models.CharField(max_length=240)
+    description = models.CharField(max_length=500)
+    reviews = models.ForeignKey('Review', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Amp(models.Model):
+    name = models.CharField(max_length=240)
+    brand = models.CharField(max_length=240)
+    #FOR WHEN WE HAVE IMAGE UPLOADS>> picture = models.ImageField(upload_to=None, height_field=None, width_fielmax_length=100)
+    wattage = models.CharField(max_length=240)
 
     def __str__(self):
         return self.name
@@ -29,8 +63,8 @@ class Sub(models.Model):
 
 class Review(models.Model):
     user = models.ForeignKey('User', on_delete=models.CASCADE)
-    title = models.CharField('Title', max_length=240)
-    body = models.CharField('Body', max_length=500)
+    title = models.CharField(max_length=240)
+    body = models.CharField(max_length=500)
     rating = models.IntegerField()
 
     def __str__(self):
