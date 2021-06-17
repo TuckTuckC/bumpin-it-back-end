9683c137170c9ed43000104cd2d9c34860215acf
TuckTuckC <mrkittay847@gmail.com>
2021-06-16 22:52:11 -0400

    added create and update to serializer

--- main_app/serializers.py
+++ main_app/serializers.py
@@ -7,3 +7,14 @@ class UserSerializer(serializers.Serializer):
     email = serializers.EmailField()
     wishlist = serializers.ForeignKey()
     registationDate = serializers.DateField()
+
+    def create(self, validated_data):
+        return User.objects.create(validated_data)
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.email = validated_data.get('email', instance.email)
+        instance.wishlist = validated_data.get('wishlist', instance.wishlist)
+        instance.registrationDate = validated_data.get('registrationDate', instance.registrationDate)
+        instance.save()
+        return instance
