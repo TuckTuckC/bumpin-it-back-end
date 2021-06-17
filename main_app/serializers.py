abbe828420f4c9ca8dd5efe7921166aee0ef5d04
TuckTuckC <mrkittay847@gmail.com>
2021-06-16 23:25:18 -0400

    converted the serializers to use meta, and added a view for requests

--- main_app/serializers.py
+++ main_app/serializers.py
@@ -1,11 +1,10 @@
 from rest_framework import serializers
-from .models import User
+from .models import (User, Sub)
 
 class UserSerializer(serializers.Serializer):
-
     name = serializers.CharField(max_length=240)
     email = serializers.EmailField()
-    wishlist = serializers.ForeignKey()
+    wishlist = serializers.RelatedField(source = 'Post', read_only = True)
     registationDate = serializers.DateField()
 
     def create(self, validated_data):
@@ -18,3 +17,25 @@ class UserSerializer(serializers.Serializer):
         instance.registrationDate = validated_data.get('registrationDate', instance.registrationDate)
         instance.save()
         return instance
+
+class SubSerializer(serializers.Serializer):
+    class Meta:
+        model = Sub
+        fields = ['id', 'name', 'brand', 'size', 'rms', 'peak']
+    # name = serializers.CharField(max_length=240)
+    # brand = serializers.CharField(max_length=240)
+    # size = serializers.CharField(max_length=240)
+    # rms = serializers.CharField(max_length=240)
+    # peak = serializers.CharField(max_length=240)
+
+    # def create(self, validated_data):
+    #     return Sub.objects.create(validated_data)
+
+    # def update(self, instance, validated_data):
+    #     instance.name = validated_data.get('name', instance.name)
+    #     instance.brand = validated_data.get('brand', instance.brand)
+    #     instance.size = validated_data.get('size', instance.size)
+    #     instance.rms = validated_data.get('rms', instance.rms)
+    #     instance.peak = validated_data.get('peak', instance.peak)
+    #     instance.save()
+    #     return instance
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from rest_framework.parsers import JSONParser
+from .models import Sub
+from .serializers import SubSerializer
 
 # Create your views here.
+
+def sub_list(request):
+    if request.method == 'GET':
+        subs = Sub.objects.all()
+        serializer = SubSerializer(subs, many = True)
+        return JsonResponse(serializer.data, safe = False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = SubSerializer(data = data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status = 201)
+        return JsonResponse(serializer.errors, status = 400)
